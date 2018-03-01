b148d9cf9c7ff9936e080f6fd6a1c6e2fadb505d
smashwin111 <ashwin111@yahoo.com>
2018-03-01 18:38:34 -0500

    Update application.js

--- application.js
+++ application.js
@@ -19,12 +19,12 @@ var submitComment = function () {
 
   var title = $("#yourName").val();
   var remark = $("#yourComment").val();
-  //var link = $("#yourLink").val();
+  var link = $("#yourLink").val();
 
   comments.push({
     "title": title,
     "remark": remark,
-    //"link": link
+    "link": link
   });
 };
 
@@ -35,7 +35,7 @@ comments.limitToLast(10).on('child_added', function(childSnapshot) {
 
   $("#title").prepend(comment.title + '<br> <br>')
   $("#remark").prepend(comment.remark + '<br> <br>')
-  //$("#link").prepend(comment.link + '<br> <br>')
+  $("#link").prepend(comment.link + '<br> <br>')
 
   // Make the link actually work and direct to the URL provided
   //$("#link").attr("href", comment.link)
