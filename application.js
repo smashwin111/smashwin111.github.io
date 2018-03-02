5824f7deeb2755896dffeededa6f51c5002d8a7b
smashwin111 <ashwin111@yahoo.com>
2018-03-01 20:46:13 -0500

    Update application.js

--- application.js
+++ application.js
@@ -35,7 +35,7 @@ comments.limitToLast(10).on('child_added', function(childSnapshot) {
 
   $("#title").prepend(comment.title + '<br> <br> <br> <br>')
   $("#remark").prepend(comment.remark + '<br> <br> <br> <br>')
-  $("#link").prepend('<img id="image" alt="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/2000px-No_image_available.svg.png">' + '<br> <br>')
+  $("#link").prepend('<img id="image" onerror="this.onerror=null;this.src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/2000px-No_image_available.svg.png';">' + '<br> <br>')
 
   // Make the link actually work and direct to the URL provided
   $("#image").attr("src", comment.link)
