fddba1ac0f145bf36d788983416eeb160b3ca91b
smashwin111 <ashwin111@yahoo.com>
2018-03-01 19:26:24 -0500

    Update application.js

--- application.js
+++ application.js
@@ -35,7 +35,7 @@ comments.limitToLast(10).on('child_added', function(childSnapshot) {
 
   $("#title").prepend(comment.title + '<br> <br> <br> <br>')
   $("#remark").prepend(comment.remark + '<br> <br> <br> <br>')
-  $("#link").prepend('<img id="image">' + '<br> <br> <br> <br>')
+  $("#link").prepend('<img id="image">' + '<br> <br>')
 
   // Make the link actually work and direct to the URL provided
   $("#image").attr("src", comment.link)
