4f3393511924a7be23bffa55617e048390928f6a
smashwin111 <ashwin111@yahoo.com>
2018-03-01 21:28:19 -0500

    Update application.js

--- application.js
+++ application.js
@@ -37,8 +37,10 @@ comments.limitToLast(10).on('child_added', function(childSnapshot) {
   $("#remark").prepend(comment.remark + '<br> <br> <br> <br>')
   $("#link").prepend('<img id="image">' + '<br> <br>')
 
+  var altWords = 'No <br> image'  
   // Make the link actually work and direct to the URL provided
   $("#image").attr("src", comment.link)
+  $("#image").attr("alt", altWords) 
 });
 
 
