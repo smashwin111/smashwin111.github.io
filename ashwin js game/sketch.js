1130e19a36192487bbbcea2dfa474c9c118b106b
smashwin111 <ashwin111@yahoo.com>
2019-05-28 14:56:05 -0400

    Update sketch.js

--- ashwin js game/sketch.js
+++ ashwin js game/sketch.js
@@ -171,3 +171,23 @@ function generate_alltime_leaderboard() {
 }
 
 generate_alltime_leaderboard()
+
+function preventPullToRefresh(element) {
+    var prevent = false;
+
+    document.querySelector(element).addEventListener('touchstart', function(e){
+      if (e.touches.length !== 1) { return; }
+
+      var scrollY = window.pageYOffset || document.body.scrollTop || document.documentElement.scrollTop;
+      prevent = (scrollY === 0);
+    });
+
+    document.querySelector(element).addEventListener('touchmove', function(e){
+      if (prevent) {
+        prevent = false;
+        e.preventDefault();
+      }
+    });
+}
+
+preventPullToRefresh("html") // pass #id or html tag into the method
