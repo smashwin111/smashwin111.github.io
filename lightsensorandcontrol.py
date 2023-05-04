cdfeaa01b36f8b3e961afb91381e1e7af89e85d6
smashwin111 <ashwin111@yahoo.com>
2023-05-03 22:55:38 -0400

    Update lightsensorandcontrol.py

--- lightsensorandcontrol.py
+++ lightsensorandcontrol.py
@@ -1,67 +1,20 @@
-# adapted from https://github.com/gallaugher/CPXBluefruitHalloweenHead/blob/master/code.py
+# Begin the main processing loop.
 
-import time
-
-# Import the board-specific input/output library.
-from adafruit_circuitplayground import cp
-
-# Import the Adafruit Bluetooth library.  Technical reference:
-# https://circuitpython.readthedocs.io/projects/ble/en/latest/api.html
-from adafruit_ble import BLERadio
-from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
-from adafruit_ble.services.nordic import UARTService
-
-#added for Day 3
+#Day 3 starts here
 from adafruit_bluefruit_connect.packet import Packet
 from adafruit_bluefruit_connect.color_packet import ColorPacket
 from adafruit_bluefruit_connect.button_packet import ButtonPacket
 
-# ----------------------------------------------------------------
-# Initialize global variables for the main loop.
-
-ble = BLERadio()
-uart = UARTService()
-advertisement = ProvideServicesAdvertisement(uart)
-
-print("Radio:", ble)
-ble.name = 'CPB-36'
-print("Radio name:", ble.name)
-
-print("Advertisement:", advertisement)
-print("UART:", uart)
-
-# Flags for detecting state changes.
-advertised = False
-connected  = False
-
-
-# The sensor sampling rate is precisely regulated using the following timer variables.
-sampling_timer    = 0.0
-last_time         = time.monotonic()
-sampling_interval = 0.10
-
-# ----------------------------------------------------------------
-# Begin the main processing loop.
-
-button4 = False
-
 while True:
-
-    # Read the accelerometer at regular intervals.  Measure elapsed time and
-    # wait until the update timer has elapsed.
     now = time.monotonic()
     interval = now - last_time
     last_time = now
     sampling_timer -= interval
     if sampling_timer < 0.0:
-        sampling_timer += sampling_interval
-        x, y, z = cp.acceleration
-        #added sound level later on
-        sound_level = cp.sound_level
-        #added outside light level later on
+        #store outside light level
         light_level = cp.light
     else:
-        x = None
+        light_level = None
 
     if not advertised:
         try:
@@ -83,7 +36,7 @@ while True:
             advertised = False
             cp.red_led = False
         else:
-            if x is not None:
+            if light_level is not None:
                 if light_level < 1:
                     uart.write("Turn on lights\n")
                     time.sleep(1.0)
